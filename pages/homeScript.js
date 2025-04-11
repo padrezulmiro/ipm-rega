2308ca3e2f166123ccc19e14f43fc75f9a2baad4
aZul <fmdcosta.piano@gmail.com>
2025-04-11 22:54:06 +0100

    feat: Add park lists HTML and functionality

---
+++ pages/homeScript.js
@@ -0,0 +1,56 @@
+const park1 = {
+  parkName: "Pq. da Paz",
+  numRegions: 11,
+  waterConsumption: 2300,
+  mapUrl: ""
+}
+
+const park2 = {
+  parkName: "Pq. Urbano Comandante Julio Ferraz",
+  numRegions: 6,
+  waterConsumption: 540,
+  mapUrl: ""
+}
+
+function main() {
+  initLocalStorage()
+  fillParkList(JSON.parse(localStorage.getItem("parks")))
+}
+
+function initLocalStorage() {
+  const isParksInStorage = localStorage.getItem("parks") !== null
+  if (!isParksInStorage) {
+    const parks = [park1, park2]
+    localStorage.setItem("parks", JSON.stringify(parks))
+  }
+}
+
+function fillParkList(parkList) {
+  const parkListDivEl = document.getElementById("park-list-div")
+  const parkLiTemplate = document.getElementById("park-list-element-template")
+  const parkListEl = document.createElement("ul")
+  parkListEl.id = "park-list"
+
+  for (let park of parkList) {
+    // build html for each park
+    const templateClone = parkLiTemplate.content.cloneNode(true)
+
+    // TODO(pip) Fill values in clone:
+    // [] link URL
+    // [] map img source URL
+    templateClone.querySelector("li.park-list-element-map > img").alt = park
+      .parkName + "'s map"
+    templateClone.querySelector("li.park-list-element-name").innerText = park
+      .parkName
+    templateClone.querySelector("li.park-list-element-nregions").innerText = park
+      .numRegions
+    templateClone.querySelector("li.park-list-element-water-max")
+                 .innerText = park.waterConsumption
+
+    parkListEl.appendChild(templateClone)
+  }
+
+  parkListDivEl.appendChild(parkListEl)
+}
+
+main()
